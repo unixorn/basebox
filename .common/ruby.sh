7f46b96d3fadd7c8ed95da0f53d11034779d218f
Tim Dysinger <tim@dysinger.net>
2012-02-29 07:49:12 -1000

    Role back to 1.9.2-p290 for now - it's the most compatible 1.9 ruby

--- .common/ruby.sh
+++ .common/ruby.sh
@@ -7,7 +7,7 @@ make && make install
 cd ..
 rm -rf *yaml*
 
-wget -O- http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz | tar oxz
+wget -O- http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz | tar oxz
 cd ruby*
 ./configure --prefix=/opt/ruby --with-opt-dir=/opt/ruby
 make && make install
