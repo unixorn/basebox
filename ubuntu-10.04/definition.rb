c3167b7deba98fe2cd60e7c97412512d677d3348
Tim Dysinger <tim@dysinger.net>
2012-02-29 07:50:38 -1000

    Use Ubuntu 10.04.4 LTS now available

--- ubuntu-10.04/definition.rb
+++ ubuntu-10.04/definition.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + "/../.ubuntu/session.rb"
 
-iso = "ubuntu-10.04.3-server-amd64.iso"
+iso = "ubuntu-10.04.4-server-amd64.iso"
 
 session =
   UBUNTU_SESSION.merge( :iso_file => iso,
-                        :iso_md5 => "84b43b7bbee85d0af8e11b778c8d1290",
-                        :iso_src => "http://releases.ubuntu.com/10.04.3/#{iso}" )
+                        :iso_md5 => "9b218654cdcdf9722171648c52f8a088",
+                        :iso_src => "http://releases.ubuntu.com/10.04.4/#{iso}" )
 
 Veewee::Session.declare session
