5fdc00744b2d54c8ee0647d75f3bfee5f501b9c8
Tim Dysinger <tim@dysinger.net>
2012-09-20 19:55:10 -1000

    Use /opt/vagrant instead of /opt/ruby. I think it's clearer.

--- .common/ruby.sh
+++ .common/ruby.sh
@@ -2,20 +2,20 @@
 
 wget -O- http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz | tar oxz
 cd yaml*
-./configure --prefix=/opt/ruby
+./configure --prefix=/opt/vagrant
 make && make install
 cd ..
 rm -rf *yaml*
 
 wget -O- http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz | tar oxz
 cd ruby*
-./configure --prefix=/opt/ruby --with-opt-dir=/opt/ruby
+./configure --prefix=/opt/vagrant --with-opt-dir=/opt/vagrant
 make && make install
-/opt/ruby/bin/gem update --system
-/opt/ruby/bin/gem update
-/opt/ruby/bin/gem clean
-/opt/ruby/bin/gem install chef puppet --no-rdoc --no-ri
+/opt/vagrant/bin/gem update --system
+/opt/vagrant/bin/gem update
+/opt/vagrant/bin/gem clean
+/opt/vagrant/bin/gem install chef puppet --no-rdoc --no-ri
 cd ..
 rm -rf *ruby*
 
-echo 'PATH=$PATH:/opt/ruby/bin' > /etc/profile.d/ruby.sh
+echo 'PATH=$PATH:/opt/vagrant/bin' > /etc/profile.d/ruby.sh
